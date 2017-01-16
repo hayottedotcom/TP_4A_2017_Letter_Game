cb5d79e91e684af9afae3188761d0204270d969d
Whiteire <cosson@et.esiea.fr>
2017-01-16 18:02:24 +0100

    +SacLettres

---
+++ LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/SacLettres.java
@@ -0,0 +1,14 @@
+package fr.esiea.unique.cosson_hayotte;
+
+public class SacLettres {
+	
+	private String[] sac={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
+			,"p","q","r","s","t","u","v","w","x","y","z"};
+
+	public SacLettres() {
+		// TODO Auto-generated constructor stub
+		
+		
+	}
+
+}
