28aaa97095444a6006905bb6341566771aa1d9c9
Florent541 <cosson@et.esiea.fr>
2017-01-17 09:09:10 +0100

    +Tirage de lettres aléatoires

--- LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/SacLettres.java
+++ LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/SacLettres.java
@@ -4,11 +4,16 @@ public class SacLettres {
 	
 	private String[] sac={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 			,"p","q","r","s","t","u","v","w","x","y","z"};
+	private double random;
 
-	public SacLettres() {
+	public SacLettres(double d) {
 		// TODO Auto-generated constructor stub
+		this.random=d;
 		
-		
+	}
+	
+	public String getLettre(){
+		return sac[(int) random];
 	}
 
 }
--- LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/main.java
+++ LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/main.java
@@ -9,6 +9,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("coucou");
+			SacLettres tirage = new SacLettres(Math.random()*25);
+			System.out.println(tirage.getLettre());
 	}
 
 }
