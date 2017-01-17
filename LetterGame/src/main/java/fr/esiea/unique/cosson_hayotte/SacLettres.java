2a881c2214eddf5bbff4647e9f0ee996fb9a071c
Florent541 <cosson@et.esiea.fr>
2017-01-17 09:18:52 +0100

    +SacLettres nouveau tirage

--- LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/SacLettres.java
+++ LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/SacLettres.java
@@ -2,16 +2,21 @@ package fr.esiea.unique.cosson_hayotte;
 
 public class SacLettres {
 	
+	//Tableau contenant les lettres
 	private String[] sac={"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o"
 			,"p","q","r","s","t","u","v","w","x","y","z"};
+	//Le nombre aléatoire généré
 	private double random;
 
-	public SacLettres(double d) {
+	public SacLettres() {
 		// TODO Auto-generated constructor stub
-		this.random=d;
 		
 	}
 	
+	public void nouveauTirage(){
+		random=Math.random() * 25;
+	}
+	//getter de la lettre en fonction du random
 	public String getLettre(){
 		return sac[(int) random];
 	}
--- LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/main.java
+++ LetterGame/src/main/java/fr/esiea/unique/cosson_hayotte/main.java
@@ -9,7 +9,13 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("coucou");
-			SacLettres tirage = new SacLettres(Math.random()*25);
+			//Déclaration objet
+			SacLettres tirage = new SacLettres();
+			//1er tirage
+			tirage.nouveauTirage();
+			System.out.println(tirage.getLettre());
+			//2ème tirage
+			tirage.nouveauTirage();
 			System.out.println(tirage.getLettre());
 	}
 
