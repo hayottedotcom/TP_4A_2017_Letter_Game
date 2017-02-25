11dc890c28c0d1063fb6ef94236a90e968e90d45
hayottedotcom <adrien@hayotte.com>
2017-02-25 16:45:32 +0100

    +french->english

--- LetterGame/src/test/java/fr/esiea/unique/cosson_hayotte/TestApp.java
+++ LetterGame/src/test/java/fr/esiea/unique/cosson_hayotte/TestApp.java
@@ -2,12 +2,17 @@ package fr.esiea.unique.cosson_hayotte;
 
 import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.regex.Pattern;
+
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestApp {
 	
 	LetterBag sl = new LetterBag();
+	Pattern p = Pattern.compile("[0-9]");
+	Boolean hasSpecialChar = false;
 
 	
 	@Before
@@ -19,12 +24,27 @@ public class TestApp {
 	public void testGetLettre() {
 		sl.newDraw();
 		String lettre = sl.getLetter();
-		assertNotNull(lettre); //Test si la lettre existe
+		assertNotNull(lettre); //If OK, letter exists.
 	}
 	
 	@Test
-	public void testScanNbJoueurs() {
-		
+	public void DicoString () {
+		Dictionary dc = new Dictionary();
+		String str = "";
+		Boolean isString = false;
+		try {
+			while ((str = dc.in.readLine()) != null) {
+			     if (hasSpecialChar = p.matcher(str).find() == true) {
+			    	 isString = false;
+			    	 break;
+			     } else {
+			    	 isString = true;
+			     }
+			 }
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(isString, true); //If OK, there are no alphanumeric characters in the Dictionary entries.
 	}
 
 }
